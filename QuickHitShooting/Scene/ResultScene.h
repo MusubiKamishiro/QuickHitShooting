51ce2aa35b171e7630c566485d9d52cbe4d8c623
musubi kamishiro <kamishiro2nd.pger@gmail.com>
2020-05-26 17:18:50 +0900

    リザルトシーンのスコア表示更新

--- QuickHitShooting/Scene/ResultScene.h
+++ QuickHitShooting/Scene/ResultScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Scene.h"
 #include <memory>
+#include <vector>
 #include <array>
 #include <string>
 #include "../Geometry.h"
@@ -22,14 +23,23 @@ class ResultScene : public Scene
 {
 private:
 	void (ResultScene::*_updater)(const Peripheral& p);
-
+	
 	void FadeinUpdate(const Peripheral& p);
 	void FadeoutUpdate(const Peripheral& p);
+	void ScoreUpdate(const Peripheral& p);
 	void WaitUpdate(const Peripheral& p);
 
 	ResultData _resultData;
 
 	std::unique_ptr<TrimString> _trimString;
+
+	int _resultScore;
+	int _time;
+	unsigned int _nowDigit;		// カウント中の桁
+	unsigned int _scoreDigit;	// スコアの桁数
+	std::vector<int> _num;
+
+	void CheckScore(const int& inscore);
 
 public:
 	ResultScene(const ResultData& resultData);
